d2420ffe4296fb3ae84be69f43b7ca084248c53c
lethang02 <cautheodang@gmail,com>
2023-05-28 22:50:54 +0700

    update 28/05/2023

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.notesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.notesapp.Adapter.NotesListAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    NotesListAdapter notesListAdapter;
+    List<Notes> notes = new ArrayList<>();
+    DBManager database;
+    FloatingActionButton fab_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.note_list);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_home);
+        fab_add = findViewById(R.id.fab_add);
+
+
     }
 }
