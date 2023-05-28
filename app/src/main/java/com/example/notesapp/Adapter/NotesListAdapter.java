64e6e67eac409f76a5bae367c1d5f15a69edf139
lethang02 <cautheodang@gmail,com>
2023-05-28 15:46:26 +0700

    update 28/05/2023

--- app/src/main/java/com/example/notesapp/Adapter/NotesListAdapter.java
+++ app/src/main/java/com/example/notesapp/Adapter/NotesListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.notesapp.Adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -11,28 +12,82 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.notesapp.Notes;
+import com.example.notesapp.NotesClickListener;
 import com.example.notesapp.R;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NotesListAdapter extends RecyclerView.Adapter<NotesViewHolder> {
     Context context;
     List<Notes>list;
+    NotesClickListener listener;
+
+    public NotesListAdapter(Context context, List<Notes> list, NotesClickListener listener) {
+        this.context = context;
+        this.list = list;
+        this.listener = listener;
+    }
 
     @NonNull
     @Override
     public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new NotesViewHolder(LayoutInflater.from(context).inflate(R.layout.note_list, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
+        holder.tv_title.setText(list.get(position).getTitle());
+        holder.tv_title.setSelected(true);
+
+        holder.tv_notes.setText(list.get(position).getNote());
+        holder.tv_date.setText(list.get(position).getDate());
+        holder.tv_date.setSelected(true);
+
+        if (list.get(position).getPinned()){
+            holder.imageView_pin.setImageResource(R.drawable.ic_pin);
+        }
+        else {
+            holder.imageView_pin.setImageResource(0);
+        }
+
+        int color_code = getRandomColor();
+        holder.notes_container.setCardBackgroundColor(holder.itemView.getResources().getColor(color_code, null));
+        holder.notes_container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onClick(list.get(holder.getAdapterPosition()));
+            }
+        });
+
+        holder.notes_container.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                listener.onLongClick(list.get(holder.getAdapterPosition()),holder.notes_container);
+                return true;
+            }
+        });
 
     }
+    private int getRandomColor(){
+        List<Integer> colorCode = new ArrayList<>();
+
+        colorCode.add(R.color.color1);
+        colorCode.add(R.color.color2);
+        colorCode.add(R.color.color3);
+        colorCode.add(R.color.color4);
+        colorCode.add(R.color.color5);
+        
+        Random random = new Random();
+        int random_color = random.nextInt(colorCode.size());
+        return random_color;
+    }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 }
 class NotesViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/example/notesapp/NotesClickListener.java
+++ app/src/main/java/com/example/notesapp/NotesClickListener.java
@@ -1,4 +1,10 @@
 package com.example.notesapp;
 
+import androidx.cardview.widget.CardView;
+
 public interface NotesClickListener {
+    void onClick(Notes notes);
+    void onLongClick(Notes notes, CardView cardView);
+
+
 }
